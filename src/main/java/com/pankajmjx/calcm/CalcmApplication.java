1aa11885df18be990e21acfe9c6ac136ef719bcc
ct50394u <pankajjoshi870.jp@gmail.com>
2022-08-29 12:18:30 +0530

    add

---
+++ src/main/java/com/pankajmjx/calcm/CalcmApplication.java
@@ -0,0 +1,13 @@
+package com.pankajmjx.calcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pankajmjx/calcm/CalcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankajmjx.calcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
